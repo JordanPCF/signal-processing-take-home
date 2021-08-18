f794127dd43c9241a5ee7ee65d5c39599686e3c8
jp <jordanpflem@gmail.com>
2021-08-17 19:36:47 -0700

    Added docstrings

--- test_convolution.py
+++ test_convolution.py
@@ -9,6 +9,7 @@ Designed for use with pytest. Contains the following tests:
         - Case: '     '    '        odd     '    '        '      '       '    even    '     '
         - Case: '     '    '        even    '    '        '      '       '    odd     '     '
         - Case: '     '    '        even    '    '        '      '       '    even    '     '
+        These cases test that the boundary effects are the same as those from the numpy function
 
 Also contains the function:
     * plot_original_and_downsampled - Compares noisy sinusoidal signal before and after downsampling
@@ -21,6 +22,10 @@ from solution import downsample_by_2, convolve_arrays
 
 class TestConvolutionFunction:
     def test_arrays_same_length(self):
+        """
+        Test that convolve_arrays has the same output as numpy's convolve() given two arrays of
+        equal length
+        """
         signal = [2, 1, -1, 3]
         conv_filter = [4, 3, 2, 1]
 
@@ -30,12 +35,21 @@ class TestConvolutionFunction:
         assert my_convolved_output == numpy_solution
 
     def test_commutative_property(self):
+        """
+        Test that the result is indifferent to the order the arrays are given.
+        This also verifies the function works in both cases where the filter array is shorter or
+        longer than the signal array.
+        """
         longer = [2, 1, -1, 3, 10]
         shorter = [4, 3, 2]
 
         assert convolve_arrays(longer, shorter) == convolve_arrays(shorter, longer)
 
     def test_boundary_effects_longer_odd_shorter_odd(self):
+        """
+        Test that convolve_arrays has the same output as numpy's convolve() given two 
+        arrays of differing length, where both input arrays have an odd number of elements.
+        """
         signal = [2, 1, -1, 3, 3, 6, 7, -3, 1, 12, 9]
         conv_filter = [4, 3, 2, 1, 6]
 
@@ -45,6 +59,11 @@ class TestConvolutionFunction:
         assert my_convolved_output == numpy_solution
 
     def test_boundary_effects_longer_odd_shorter_even(self):
+        """
+        Test that convolve_arrays has the same output as numpy's convolve() given two 
+        arrays of differing length, where the longer input array has an odd number of elements, 
+        and the shorter array an even number of elements. 
+        """
         signal = [2, 1, -1, 3, 3, 6, 7, -3, 1, 12, 9]
         conv_filter = [4, 3, 2, 1]
 
@@ -54,6 +73,11 @@ class TestConvolutionFunction:
         assert my_convolved_output == numpy_solution
 
     def test_boundary_effects_longer_even_shorter_odd(self):
+        """
+        Test that convolve_arrays has the same output as numpy's convolve() given two 
+        arrays of differing length, where the longer input array has an even number of elements, 
+        and the shorter array an odd number of elements. 
+        """
         signal = [2, 1, -1, 3, 3, 6, 7, -3, 1, 12, 9, 41]
         conv_filter = [4, 3, 2, 1, 6]
 
@@ -63,6 +87,10 @@ class TestConvolutionFunction:
         assert my_convolved_output == numpy_solution
 
     def test_boundary_effects_longer_even_shorter_even(self):
+        """
+        Test that convolve_arrays has the same output as numpy's convolve() given two 
+        arrays of differing length, where both input arrays have an even number of elements.
+        """
         signal = [2, 1, -1, 3, 3, 6, 7, -3, 1, 12, 9, 41]
         conv_filter = [4, 3, 2, 1, 6, 10]
 
@@ -72,6 +100,12 @@ class TestConvolutionFunction:
         assert my_convolved_output == numpy_solution
 
 def plot_original_and_downsampled():
+    """
+    A 'visual test' to see the efficacy of the function downsample_by_2
+
+    A noisy sinusoid is plotted before and after the application of the low-pass filter and
+    decimation.
+    """
 
     t = np.linspace(1, 100, 1000)
     t_decimated = np.linspace(1, 100, 500)
